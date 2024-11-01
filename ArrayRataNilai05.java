12159baa6df7d7c383632dbc54213a7ce491d8c5
CitraAyu0 <meilindacitraayu@gmail.com>
2024-11-02 01:06:35 +0700

    modifikasi percobaan

--- ArrayRataNilai05.java
+++ ArrayRataNilai05.java
@@ -3,18 +3,30 @@ public class ArrayRataNilai05 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
-        int[] nilaiMhs = new int[10];
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMhs];
         double total = 0;
         double rata2;
 
         for (int i=0; i<nilaiMhs.length; i++){
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+ ": ");
             nilaiMhs[i] = sc.nextInt();
         }
             for (int i=0; i<nilaiMhs.length; i++){
-                total += nilaiMhs[i];
+                if (nilaiMhs[i] > 70) {
+                    System.out.println("Nilai akhir mahasiswa ke-" +(i+1)+ " lulus!");
+                }else{
+                    System.out.println("Nilai akhir mahasiswa ke-" +(i+1)+ " tidak lulus!");
+                }
             }
-             rata2 = total/nilaiMhs.length;
-             System.out.println("Rata-rata nilai = " +rata2);
+                for (int i=0; i<nilaiMhs.length; i++){
+                    total += nilaiMhs[i];
+                }
+                rata2 = total/nilaiMhs.length;
+                System.out.println("Rata-rata nilai lulus = " +rata2);
+                System.out.println("Rata-rata nilai tidak lulus = " +rata2);
     }
 }
